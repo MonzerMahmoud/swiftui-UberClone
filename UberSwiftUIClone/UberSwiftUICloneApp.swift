7597a34e109f1f8a5136f8585505ab1699ccf22b
MonzerMahmoud <55148623+MonzerMahmoud@users.noreply.github.com>
2021-12-31 11:28:14 +0200

    Initial Commit

---
+++ UberSwiftUIClone/UberSwiftUICloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUICloneApp.swift
+//  UberSwiftUIClone
+//
+//  Created by Syber Expertise on 31/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUICloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
